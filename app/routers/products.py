7ab126a2065646b5f2add3a33990526336f20c8d
matmar435 <matmar435@gmail.com>
2026-01-16 14:21:23 +0100

    add endpoint add_product

--- app/routers/products.py
+++ app/routers/products.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.database import SesionLocal
-from app.crud.product import get_products
+from app.crud.product import get_products, create_product
+from app.schemas import ProductResponse, ProductCreate
 
 router = APIRouter(
     prefix="/products",
@@ -18,8 +19,15 @@ def get_db():
         db.close()
 
 
-@router.get("/pro")
+@router.get("/", response_model=list[ProductResponse])
 def read_products(db: Session = Depends(get_db)):
     return get_products(db)
+
+@router.post("/", response_model=list[ProductResponse])
+def add_product(
+        product: ProductCreate,
+        db: Session = Depends(get_db)
+):
+    return create_product(db,product)
 
 
