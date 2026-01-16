236d4502625bacab88b97d5474c499f0a95ee553
matmar435 <matmar435@gmail.com>
2026-01-16 17:53:59 +0100

    aply union

--- app/services/forecast.py
+++ app/services/forecast.py
@@ -1,8 +1,10 @@
+from typing import Union
+
 import pandas as pd
 from datetime import timedelta
 
 
-def prepare_dataframe(data: list | [dict]):
+def prepare_dataframe(data: Union[list, dict]):
     df = pd.DataFrame(data)
     df["date"] = pd.to_datetime(df["date"])
     df = df.sort_values("date")
