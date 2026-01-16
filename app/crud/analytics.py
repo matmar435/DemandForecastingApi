f3a89ba7a2bdc4c505cf07c149823ffa94ffc41f
matmar435 <matmar435@gmail.com>
2026-01-16 17:18:06 +0100

    add dailyDemandByProduct

--- app/crud/analytics.py
+++ app/crud/analytics.py
@@ -20,3 +20,23 @@ def total_quantity_by_product(db: Session):
         }
         for Order.product_id, total_quantity in results
     ]
+
+def daily_demand_by_product(db:Session,product_id:int):
+    results = (
+        db.query(
+            Order.order_date,
+            func.sum(Order.quantity).label("daily_quantity")
+        )
+        .filter(Order.product_id == product_id)
+        .group_by(Order.order_date)
+        .order_by(Order.order_date)
+        .all()
+    )
+
+    return [
+        {
+            "date": order_date,
+            "quantity": quantity
+        }
+        for order_date, quantity in results
+    ]
