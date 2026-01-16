c585ac7ad40c171f62c115b8624009513314d42b
matmar435 <matmar435@gmail.com>
2026-01-16 14:17:09 +0100

    add create_product in crud

--- app/crud/product.py
+++ app/crud/product.py
@@ -1,5 +1,17 @@
 from sqlalchemy.orm import Session
 from app.models.product import Product
+from app.schemas import ProductCreate
+
 
 def get_products(db: Session):
     return db.query(Product).all()
+
+def create_product(db:Session,product:ProductCreate):
+    db_product = Product(
+        name=product.name,
+        category=product.category
+    )
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
