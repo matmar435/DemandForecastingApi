3c0b43898bdbea91ec0cca65d33eeb4e5b864e03
matmar435 <matmar435@gmail.com>
2026-01-16 17:21:53 +0100

    add dailyDemandEndpoint

--- app/routers/analytics.py
+++ app/routers/analytics.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.crud.analytics import total_quantity_by_product
+from app.crud.analytics import total_quantity_by_product, daily_demand_by_product
 from app.database import SesionLocal
 from app.schemas import TotalQuantityResponse
 
@@ -18,3 +18,8 @@ def get_db():
 @router.get("/total-quantity", response_model=list[TotalQuantityResponse])
 def total_quantity(db: Session = Depends(get_db)):
     return total_quantity_by_product(db)
+
+
+@router.get("/daily-demand/{product_id}")
+def daily_demand(product_id: int, db: Session = Depends(get_db())):
+    return daily_demand_by_product(db, product_id)
