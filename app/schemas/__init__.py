84273e2265b41d858ee1354677c3742ddd227595
matmar435 <matmar435@gmail.com>
2026-01-16 15:45:16 +0100

    add schemas analytisc

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,3 +2,4 @@ from .product_create import ProductCreate
 from .product_response import ProductResponse
 from .order_create import OrderCreate
 from .order_response import OrderResponse
+from .analytisc import TotalQuantityResponse
